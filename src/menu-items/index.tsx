601b2228a0c563d0979d330aef5f675848d1e58d
passione-25 <biz725@proton.me>
2026-03-19 14:58:08 -0500

    removed utilities

--- src/menu-items/index.tsx
+++ src/menu-items/index.tsx
@@ -1,7 +1,6 @@
 // project import
 import dashboard from './dashboard';
 import pages from './page';
-import utilities from './utilities';
 import support from './support';
 
 // types
@@ -10,7 +9,7 @@ import { MenuItems } from '../types/menu';
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems: MenuItems = {
-  items: [dashboard, pages, utilities, support]
+  items: [dashboard, pages, support]
 };
 
 export default menuItems;
